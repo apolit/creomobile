a675f679386c8a6171eb2549409b27b8e066ffad
Artur Polit <artur@creocode.com>
2012-05-02 23:14:55 +0200

    generated sample Android application

---
+++ CatalogAndroid/src/com/creocode/catalog/android/sampleApplication/SampleApplicationActivity.java
@@ -0,0 +1,8 @@
+
+package com.creocode.catalog.android.sampleApplication;
+
+import com.creocode.catalog.android.CategoriesActivity;
+
+public class SampleApplicationActivity extends CategoriesActivity {
+
+}
